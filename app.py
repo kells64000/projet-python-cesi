8ee7e2e2d08a83377ef8b066f5ad41e1de7bb170
atruscello <alexis.truscello@viacesi.fr>
2019-04-08 12:02:15 +0200

    Get BDD

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
+import mysql.connector
+from mysql.connector import Error
+import pymysql
 
 app = Flask(__name__)
 
+class Database:
+    def __init__(self):
+        host = "127.0.0.1"
+        user = "root"
+        password = ""
+        db = "cesimad"
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
+                                   DictCursor)
+        self.cur = self.con.cursor()
+    def list_employees(self):
+        self.cur.execute("SELECT cli_prenom,cli_nom FROM customer LIMIT 50")
+        result = self.cur.fetchall()
+        return result
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def employees():
+    def db_query():
+        db = Database()
+        emps = db.list_employees()
+        return emps
+    res = db_query()
+    return render_template('index.html', result=res, content_type='application/json')
 
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
