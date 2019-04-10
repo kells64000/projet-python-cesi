72e91cae34555d7656d45c038605878c04ca8d51
kells <kelian.bousquet@laposte.net>
2019-04-10 17:47:17 +0200

    bug js function onclick ??

--- static/js/rename_sensor.js
+++ static/js/rename_sensor.js
@@ -1,37 +1,44 @@
-// $( document ).ready(function() {
-//
-//     $(".fa-edit").click(function(e) {
-//         e.stopPropagation();
-//         $(this).parent().hide();
-//         $(this).parent().closest("div").append("" +
-//             "<div class=\"field has-addons has-text-centered\">\n" +
-//             "  <div class=\"control\">\n" +
-//             "    <input class=\"input\" type=\"text\" placeholder=\"nouveau nom\">\n" +
-//             "  </div>\n" +
-//             "  <div class=\"control\">\n" +
-//             "    <a class=\"button is-success\">\n" +
-//             "      <i class=\"fas fa-check\"></i>\n" +
-//             "    </a>\n" +
-//             "    <a class=\"button is-danger\" onclick=\"cancelRename()\">\n" +
-//             "      <i class=\"fas fa-times\"></i>\n" +
-//             "    </a>\n" +
-//             "  </div>\n" +
-//             "</div>");
-//
-//     });
-//
-// });
+function editName() {
 
-document.addEventListener("DOMContentLoaded", function (event) {
+    e.parentNode.style.display = 'none';
 
-    var edits = document.getElementsByClassName('fa-edit');
-    for (var i = 0; i < edits.length; i++) {
+    console.log(e);
 
-        var edit = edits[i];
-        edit.onclick = function () {
+    console.log(e.val());
 
-            this.parentNode.style.display = 'none';
-            this.parentNode.prepend("<div>test</div>");
-        }
-    }
-});
+
+    // var theFirstChild = document.getElementById()
+    //
+    // console.log(theFirstChild);
+
+    // Create a new element
+    var input = document.createElement("div");
+
+    input.classList.add("field");
+    input.classList.add("has-addons");
+    input.classList.add("has-text-centered");
+
+    input.innerHTML = "" +
+        "<div class='control'>" +
+        "<input class='input' type='text' placeholder='nouveau nom'>" +
+        "</div>" +
+        "<div class='control'>" +
+        "<a class='button is-success'>" +
+        "<i class='fas fa-check'></i>" +
+        "</a>" +
+        "<a class='button is-danger' onclick='cancelRename()'>" +
+        "<i class='fas fa-times'></i>" +
+        "</a>" +
+        "</div>" +
+        "</div>";
+
+    // Insert the new element before the first child
+    // this.parentNode.insertBefore(input, theFirstChild);
+
+
+    /*            var request = new XMLHttpRequest();
+                request.open('POST', '/sensor/', true);
+                request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+                request.send(data);*/
+
+}
