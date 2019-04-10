a659eebb63f82f4a917ed12e25a5bd1892268762
kells <kelian.bousquet@laposte.net>
2019-04-10 18:58:45 +0200

    syntax script sql

--- db/database.sql
+++ db/database.sql
@@ -28,9 +28,9 @@ CREATE TABLE IF NOT EXISTS `sensor` (
   `id_sensor` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(55) NOT NULL,
   `ID` int(11) NOT NULL,
-  `Mac` varchar(17) NOT NULL
+  `Mac` varchar(17) NOT NULL,
   PRIMARY KEY (`id_sensor`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 
 -- --------------------------------------------------------
@@ -50,14 +50,14 @@ CREATE TABLE IF NOT EXISTS `data_sensor` (
   `id_sensor` int(11) NOT NULL,
   PRIMARY KEY (`id_data_sensor`),
   KEY `id_sensor` (`id_sensor`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `weather_api`;
 CREATE TABLE IF NOT EXISTS `weather_api` (
   `id_weather_api` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(55) NOT NULL,
   PRIMARY KEY (`id_weather_api`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `data_weather_api`;
 CREATE TABLE IF NOT EXISTS `data_weather_api` (
@@ -69,4 +69,4 @@ CREATE TABLE IF NOT EXISTS `data_weather_api` (
   `id_weather_api` int(11) NOT NULL,
   PRIMARY KEY (`id_data_weather_api`),
   KEY `id_sensor` (`id_weather_api`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
